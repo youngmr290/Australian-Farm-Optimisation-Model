fdb1129490fabdf4af788f1efb3fb97f9931880f
Michael Young <Youngmr44@gmail.com>
2020-11-07 20:11:31 +0800

    fix error with dtype in update function

--- Functions.py
+++ Functions.py
@@ -652,8 +652,14 @@ def f_update(existing_value, new_value, mask_for_new):
         returns a combination of the two input arrays determined by the mask. Note: multiplying by true return the origional number and multiplying by false results in 0.
 
     '''
-    new = existing_value * np.logical_not(mask_for_new) + new_value * mask_for_new #used not rather than ~ because ~False == -1 not True (not the case for np.arrays only if bool is single - as it is for sire in some situatoins)
-    return new.astype(existing_value.dtype) #convert back to origional dtype because adding float32 and int32 returns float64. And sometimes we dont want this eg postprocessing
+    updated = existing_value * np.logical_not(mask_for_new) + new_value * mask_for_new #used not rather than ~ because ~False == -1 not True (not the case for np.arrays only if bool is single - as it is for sire in some situatoins)
+    ##sometimes a single int is update eg in the first itteration on generator. this causes error because only numpy arrays have .dtype
+    try:
+        updated = updated.astype(
+            existing_value.dtype)  # convert back to origional dtype because adding float32 and int32 returns float64. And sometimes we dont want this eg postprocessing
+    except AttributeError:
+        pass
+    return updated
 
 
 
