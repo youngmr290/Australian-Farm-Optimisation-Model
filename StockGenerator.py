fix offs day per period alter sup feed formula alter sup feed inputs

StockGenerator.py
@@ -1093,7 +1093,7 @@ def generator(params,report):
     days_period_pa1e1b1nwzida0e0b0xyg0 = age_end_pa1e1b1nwzida0e0b0xyg0 +1 - age_start_pa1e1b1nwzida0e0b0xyg0
     days_period_pa1e1b1nwzida0e0b0xyg1 = age_end_pa1e1b1nwzida0e0b0xyg1 +1 - age_start_pa1e1b1nwzida0e0b0xyg1
     days_period_pa1e1b1nwzida0e0b0xyg2 = age_end_pa1e1b1nwzida0e0b0xyg2 +1 - age_start_pa1e1b1nwzida0e0b0xyg2
-    days_period_pa1e1b1nwzida0e0b0xyg3 = age_end_pa1e1b1nwzida0e0b0xyg3 +1 - age_start_pa1e1b1nwzida0e0b0xyg3
+    days_period_pa1e1b1nwzida0e0b0xyg3 = (age_end_pa1e1b1nwzida0e0b0xyg3 +1 - age_start_pa1e1b1nwzida0e0b0xyg3)*mask_p_offs_p #mul by mask to make days per period 0 for period offs is not simulated
     days_period_cut_pa1e1b1nwzida0e0b0xyg3 = days_period_pa1e1b1nwzida0e0b0xyg3[mask_p_offs_p] #masked version of p axis
 
     ##Age of foetus (start of period, end of period and mid period - days)
@@ -1632,7 +1632,7 @@ def generator(params,report):
 
 
     ## Loop through each week of the simulation (p) for ewes
-    for p in range(180):
+    for p in range(190):
     #for p in range(n_sim_periods):
         print(p)
         if np.any(period_is_birth_pa1e1b1nwzida0e0b0xyg1[p]):

SupFeed.py
@@ -104,16 +104,16 @@ def sup_md_vol(params):
     ##calc vol
     sup_md_vol = uinp.supfeed['sup_md_vol']    
     ###convert md to dmd
-    dmd=(sup_md_vol.loc['energy']/1000+2)/17 #rearanged version of dmd to md formula in feed budget
+    dmd=(sup_md_vol.loc['energy']/1000).apply(fun.md_to_dmd)
     ##calc relative quality - note that the equation system used is the one selected for dams in p1 - currently only cs function exists
     if pinp.sheep['i_eqn_used_g1_q1p7'][6,0]==0: #csiro function used
         rq = sfun.f_rq_cs(dmd,0)
     ###use max(1,...) to make it the same as midas - this increases lupin vol slightly from what the equation returns
     vol_kg=np.maximum(1,1/rq)
     ###convert vol per kg to per tonne fed - have to adjust for the actual dry matter content and wastage
-    vol_tonne=vol_kg*1000*sup_md_vol.loc['prop consumed']/100*sup_md_vol.loc['dry matter content']/100
+    vol_tonne=vol_kg*1000*sup_md_vol.loc['prop consumed']*sup_md_vol.loc['dry matter content']
     ##calc ME
-    md_tonne=sup_md_vol.loc['energy']*sup_md_vol.loc['prop consumed']/100*sup_md_vol.loc['dry matter content']/100
+    md_tonne=sup_md_vol.loc['energy']*sup_md_vol.loc['prop consumed']*sup_md_vol.loc['dry matter content']
     ##load into params dict for pyomo
     params['vol_tonne'] = vol_tonne.to_dict()
     params['md_tonne'] = md_tonne.to_dict()