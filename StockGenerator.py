640535091aa144adb6cb50502f47300e0b8d378a
Michael <youngmr44@gmail.com>
2021-01-10 12:34:38 +0800

    push with memory fixes

--- StockGenerator.py
+++ StockGenerator.py
@@ -57,8 +57,8 @@ import sys,traceback
 
 
 
-from memory_profiler import profile
-@profile
+# from memory_profiler import profile
+# @profile
 def generator(params,r_vals,plots = False):
     """
     A function to wrap the generator and post processing that can be called by SheepPyomo.
