e2e73cdb2a3e164117d7281052f8c5ca200b0920
Michael <youngmr44@gmail.com>
2021-01-12 11:49:09 +0800

    get internet example working

---
+++ Runef.py
@@ -0,0 +1,9 @@
+'''
+Module used to construct lp file from discrete model and stocastic add ons.
+'''
+
+
+
+import subprocess
+
+subprocess.call(['C:/Users/21512438/Anaconda3/Scripts/runef.exe', '--symbolic-solver-labels', '-mReferenceModel.py'])
