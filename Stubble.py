3a997954ba5319ba8692c99ad95dd153c868f8c8
Michael <youngmr44@gmail.com>
2021-02-01 08:37:16 +0800

    finetune Stubble.py

--- Stubble.py
+++ Stubble.py
@@ -44,8 +44,10 @@ def stubble_all(params):
 
     ##create mask which is stubble available. Stubble is available from the period harvest starts to the beginning of the following growing season.
     ##if the end date of the fp is after harvest then stubble is available.
-    harv_date_k = pinp.crop['start_harvest_crops'].values.astype('datetime64[D]')
-    mask_stubble_exists_p6zk = per.f_feed_periods().values[1:,:,na] > harv_date_k  #need to use the full fp array that has the end date of the last period.
+    fp_end_p6z = per.f_feed_periods()[1:].astype('datetime64[D]')
+    fp_start_p6z = per.f_feed_periods()[:-1].astype('datetime64[D]')
+    harv_date_zk = pinp.f_seasonal_inp(pinp.crop['start_harvest_crops'].values.astype('datetime64[D]'), numpy=True, axis=1).swapaxes(0,1)
+    mask_stubble_exists_p6zk = fp_end_p6z[...,na] > harv_date_zk  #need to use the full fp array that has the end date of the last period.
 
     #########################
     #dmd deterioration      #
@@ -53,9 +55,7 @@ def stubble_all(params):
     stubble_per_grain = crp.f_stubble_production() #produces dict with stubble production per kg of yield for each grain used in the ri.availability section
 
     ##days since harvest (calculated from the end date of each fp)
-    fp_end_p6z = per.f_feed_periods().iloc[1:].values.astype('datetime64[D]')
-    fp_start_p6z = per.f_feed_periods().iloc[:-1].values.astype('datetime64[D]')
-    days_since_harv_p6zk = fp_end_p6z[...,na] - harv_date_k
+    days_since_harv_p6zk = fp_end_p6z[...,na] - harv_date_zk
     days_since_harv_p6zk[days_since_harv_p6zk.astype(int)<0] = days_since_harv_p6zk[days_since_harv_p6zk.astype(int)<0] + 365  #add 365 to the periods at the start of the year becasue as far as stubble goes they are after harvest
     average_days_since_harv_p6zk = days_since_harv_p6zk - np.minimum(days_since_harv_p6zk, (fp_end_p6z - fp_start_p6z)[...,na])/2 #subtract half the length of current period to get the average days since harv. Minimum is to handle the period when harvest occurs.
     average_days_since_harv_p6zk = average_days_since_harv_p6zk.astype(float)
@@ -85,6 +85,7 @@ def stubble_all(params):
     n_crops = len(pinp.crop['start_harvest_crops'])
     n_comp = dmd_component_harv_ks0.shape[1]
     n_cat = 4
+    n_seasons = pinp.f_keys_z().shape[0]
     ks0s1 = (n_crops, n_comp, n_cat)
     stub_cat_component_proportion_ks0s1 = np.zeros(ks0s1)
     for crop, crop_idx in zip(pinp.crop['start_harvest_crops'].index, range(len(pinp.crop['start_harvest_crops']))):
@@ -103,20 +104,20 @@ def stubble_all(params):
     ##ri availability - first calc stubble foo (stub available) this is the average from all rotations because we just need one value for foo
     ###try calc the base yield for each crop but if the crop is not one of the rotation phases then assign the average foo (this is only to stop error. it doesnt matter because the crop doesnt exist so the stubble is never used)
     base_yields = crp.f_base_yield().droplevel(0, axis=0) #drop rotation index
-    stub_foo_harv_k = np.zeros(n_crops)
+    stub_foo_harv_zk = np.zeros((n_seasons, n_crops))
     for crop, crop_idx in zip(pinp.crop['start_harvest_crops'].index, range(len(pinp.crop['start_harvest_crops']))):
         try:
-            stub_foo_harv_k[crop_idx] = base_yields.loc[crop].mean() * stubble_per_grain[(crop,'a')]
+            stub_foo_harv_zk[:,crop_idx] = base_yields.loc[crop].mean() * stubble_per_grain[(crop,'a')]
         except KeyError: #if the crop is not in any of the rotations assign average foo to stop error - this is not used so could assign any value.
-            stub_foo_harv_k[crop_idx] = base_yields.mean()
+            stub_foo_harv_zk[:,crop_idx] = base_yields.mean()
     ###adjust the foo for each catergory becasue the good stuff is eaten first therefore there is less foo when the sheep start eating the poorer stubble
     cat_propn_ks1 = pinp.stubble['stub_cat_prop']
     cat_propn_rolled_ks1 = np.roll(cat_propn_ks1, shift=1, axis=1) #roll along the cat axis. So that the previous cat lines up with the current cat
     cat_propn_rolled_ks1[:,0] = 0 #set the first slice to 0 becasue no stubble is consumed before cat A is consumed eg there is 100% of foo available when sheep are consuming cat A
     cat_cum_propn_ks1 = np.cumsum(cat_propn_rolled_ks1, axis=1) #cumulative sum of the component sizes.
-    stubble_foo_ks1 = stub_foo_harv_k[:,na] *  (1 - cat_cum_propn_ks1)
+    stubble_foo_zks1 = stub_foo_harv_zk[...,na] *  (1 - cat_cum_propn_ks1)
     ###adjust for quantity delcine due to deterioration
-    stubble_foo_p6zks1 = stubble_foo_ks1 * (1 - quant_decline_p6zk[...,na])
+    stubble_foo_p6zks1 = stubble_foo_zks1 * (1 - quant_decline_p6zk[...,na])
     ###ri availiabilty
     if uinp.sheep['i_eqn_used_g1_q1p7'][5,0]==0: #csiro function used - note that the equation system used is the one selected for dams in p1
         ri_availability_p6zks1 = sfun.f_ra_cs(stubble_foo_p6zks1, pinp.stubble['i_hf'])
@@ -159,7 +160,7 @@ def stubble_all(params):
     ###############
     #how far through each period does harv start? note: 0 for each period harv doesn't start in. Used to calc stub consumption limit in harv period
     fp_len_p6z = fp_end_p6z - fp_start_p6z
-    cons_propn_p6zk = np.clip((fp_len_p6z[...,na] - (fp_end_p6z[...,na] - harv_date_k[na,na,:])) / fp_len_p6z[...,na],0, np.inf)
+    cons_propn_p6zk = np.clip((fp_len_p6z[...,na] - (fp_end_p6z[...,na] - harv_date_zk[na,na,:])) / fp_len_p6z[...,na],0, np.inf)
     cons_propn_p6zk[cons_propn_p6zk>1] = 0
 
     #########
@@ -170,7 +171,7 @@ def stubble_all(params):
     keys_s1_cut = np.array(['b', 'c'])
     keys_s1_cut2 = np.array(['a'])
     keys_s1 = pinp.stubble['stub_cat_idx']
-    keys_p6 = np.asarray(per.f_feed_periods().index[:-1])
+    keys_p6 = pinp.period['i_fp_idx']
     keys_z = pinp.f_keys_z()
 
 
