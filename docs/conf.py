a86fc5988c9d23b4b63a6578aa618a37ce45fdf7
Michael <youngmr44@gmail.com>
2021-05-15 14:12:29 +0800

    attempt 2 to get readthedocs to install package

--- docs/conf.py
+++ docs/conf.py
@@ -10,7 +10,6 @@
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 #
-import sphinxcontrib.bibtex
 import os
 import sys
 sys.path.insert(0, os.path.abspath('..'))
