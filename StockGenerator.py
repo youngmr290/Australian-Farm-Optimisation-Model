e87362a424933ee48ba7578eda127c82d4a5184c
Michael <youngmr44@gmail.com>
2021-09-17 10:12:15 +0800

    v2 of adding the within season transfer stuff to StockGenerator.py

--- StockGenerator.py
+++ StockGenerator.py
@@ -5750,6 +5750,72 @@ def generator(params,r_vals,nv,plots = False):
                                                          * (dvp_type_next_va1e1b1nwzida0e0b0xyg1 == prejoin_vtype1)
                                                          * dry_sales_forced_va1e1b1nwzida0e0b0xyg1)
 
+    ###############################
+    # Create Season transfer mask #
+    ###############################
+    '''If a season is not identified then it does not transfer any parameters. Therefore to reduce size we can mask
+    all parameters with a z8 axis. We also require a z8z9 mask which controls transfer params'''
+
+    ##inputs
+    date_initiate_z = pinp.f_seasonal_inp(pinp.general['i_date_initiate_z'], numpy=True, axis=0).astype('datetime64')
+    date_initiate_zidaebxyg = fun.f_expand(date_initiate_z, z_pos)
+    index_zidaebxyg = fun.f_expand(index_z, z_pos)
+    start_of_season_zidaebxyg = date_node_zidaebxygm[...,0]
+    end_of_season_zidaebxyg = start_of_season_zidaebxyg + np.timedelta64(364,'D') #use 364 because end date is the day before brk.
+
+    ##parent z
+    date_prev_node_zidaebxygm = np.roll(date_node_zidaebxygm, axis=-1, shift=1)
+    existing_season_prev_zidaebxygm = np.maximum.accumulate((date_prev_node_zidaebxygm==date_initiate_zidaebxyg[...,na]) * index_zidaebxyg[...,na], axis=0)
+    existing_season_prev_zidaebxygm[:,...,0] = np.maximum.accumulate((date_node_zidaebxygm==date_initiate_zidaebxyg[...,na]) * index_zidaebxyg[...,na], axis=0)[:,...,0] #the perent at the first node are the seasons identified by break
+    parent_zidaebxyg = np.max(np.maximum.accumulate(existing_season_prev_zidaebxygm * (date_node_zidaebxygm==date_initiate_zidaebxyg[...,na]), axis=0),axis=-1)
+    parent_idaebxygz9 = np.moveaxis(parent_zidaebxyg, source=0, destination=-1)
+
+    ##number req mask. Each z8 always requires from the same z9 season eg z8[1] requires from z9[1]
+    identity_z8ida0e0b0xygz9 = fun.f_expand(np.identity(len_z),z_pos-1, right_pos=-1)
+    mask_param_reqz8z9_z8z9 = identity_z8ida0e0b0xygz9
+
+    ##dams child parent transfer
+    ###adjust dvp start dates to the base yr (dates must be between break of current season and break of next season)
+    add_yrs = np.ceil(np.maximum(0,(start_of_season_zidaebxyg - dvp_start_va1e1b1nwzida0e0b0xyg1).astype('timedelta64[D]').astype(int) / 365))
+    sub_yrs = np.ceil(np.maximum(0,(dvp_start_va1e1b1nwzida0e0b0xyg1 - end_of_season_zidaebxyg).astype('timedelta64[D]').astype(int) / 365))
+    date_nodes_va1e1b1nwzida0e0b0xyg1 = dvp_start_va1e1b1nwzida0e0b0xyg1 + add_yrs * np.timedelta64(365, 'D') - sub_yrs * np.timedelta64(365, 'D')
+    ###mask when season is identified
+    #todo this mask should be applied to all params with an active v & z axis (same as mask_w8vars_va1e1b1nw8zida0e0b0xyg1)
+    mask_z8var_va1e1b1nwzida0e0b0xyg1 = date_initiate_zidaebxyg <= date_nodes_va1e1b1nwzida0e0b0xyg1
+
+    ###dams numbers prov mask. Parent seasons provide to child season until the child season is identified.
+    prov_self_va1e1b1nwzida0e0b0xyg1z9 = mask_z8var_va1e1b1nwzida0e0b0xyg1[...,na] * identity_z8ida0e0b0xygz9
+    prov_child_va1e1b1nwzida0e0b0xyg1z9 = mask_z8var_va1e1b1nwzida0e0b0xyg1[...,na] * (index_zidaebxyg[...,na] == parent_idaebxygz9)
+    mask_z9var_va1e1b1nwzida0e0b0xyg1z = np.swapaxes(mask_z8var_va1e1b1nwzida0e0b0xyg1[...,na], z_pos-1, -1)
+    prov_child_va1e1b1nwzida0e0b0xyg1z9 = prov_child_va1e1b1nwzida0e0b0xyg1z9 * np.logical_not(mask_z9var_va1e1b1nwzida0e0b0xyg1z) #parent seasons only provide to child until child is identified
+    mask_param_provz8z9_va1e1b1nwzida0e0b0xyg1z9 = np.logical_or(prov_self_va1e1b1nwzida0e0b0xyg1z9, prov_child_va1e1b1nwzida0e0b0xyg1z9)
+    ###cluster e and b (e axis is active from the dvp dates)
+    mask_param_provz8z9_k2tva1e1b1nwzida0e0b0xyg1z9 = np.sum(mask_param_provz8z9_va1e1b1nwzida0e0b0xyg1z9
+                                                          * (a_k2cluster_va1e1b1nwzida0e0b0xyg1==index_k2tva1e1b1nwzida0e0b0xyg1)[...,na],
+                                                          axis=(e1_pos-1,b1_pos-1), keepdims=True)
+
+    ##offs child parent transfer
+    ###adjust dvp start dates to the base yr (dates must be between break of current season and break of next season)
+    add_yrs = np.ceil(np.maximum(0,(start_of_season_zidaebxyg - dvp_start_va1e1b1nwzida0e0b0xyg3).astype('timedelta64[D]').astype(int) / 365))
+    sub_yrs = np.ceil(np.maximum(0,(dvp_start_va1e1b1nwzida0e0b0xyg3 - end_of_season_zidaebxyg).astype('timedelta64[D]').astype(int) / 365))
+    date_nodes_va1e1b1nwzida0e0b0xyg3 = dvp_start_va1e1b1nwzida0e0b0xyg3 + add_yrs * np.timedelta64(365, 'D') - sub_yrs * np.timedelta64(365, 'D')
+    ###mask when season is identified
+    #todo this mask should be applied to all params with an active v & z axis (same as mask_w8vars_va1e1b1nw8zida0e0b0xyg3)
+    mask_z8var_va1e1b1nwzida0e0b0xyg3 = date_initiate_zidaebxyg <= date_nodes_va1e1b1nwzida0e0b0xyg3
+    ###offs numbers prov mask. Parent seasons provide to child season until the child season is identified.
+    prov_self_va1e1b1nwzida0e0b0xyg3z9 = mask_z8var_va1e1b1nwzida0e0b0xyg3[...,na] * identity_z8ida0e0b0xygz9
+    prov_child_va1e1b1nwzida0e0b0xyg3z9 = mask_z8var_va1e1b1nwzida0e0b0xyg3[...,na] * (index_zidaebxyg[...,na] == parent_idaebxygz9)
+    mask_z9var_va1e1b1nwzida0e0b0xyg3z = np.swapaxes(mask_z8var_va1e1b1nwzida0e0b0xyg3[...,na], z_pos-1, -1)
+    prov_child_va1e1b1nwzida0e0b0xyg3z9 = prov_child_va1e1b1nwzida0e0b0xyg3z9 * np.logical_not(mask_z9var_va1e1b1nwzida0e0b0xyg3z) #parent seasons only provide to child until child is identified
+    mask_param_provz8z9_va1e1b1nwzida0e0b0xyg3z9 = np.logical_or(prov_self_va1e1b1nwzida0e0b0xyg3z9, prov_child_va1e1b1nwzida0e0b0xyg3z9)
+    ###cluster d (d axis is active from the dvp dates)
+    mask_param_provz8z9_k3k5tva1e1b1nwzida0e0b0xyg3z9 = np.sum(mask_param_provz8z9_va1e1b1nwzida0e0b0xyg3z9
+                                                          * (a_k3cluster_da0e0b0xyg3 == index_k3k5tva1e1b1nwzida0e0b0xyg3)[...,na],
+                                                          axis=d_pos-1, keepdims=True)
+
+
+
+
     ###########################
     #create production params #
     ###########################
@@ -6136,94 +6202,45 @@ def generator(params,r_vals,nv,plots = False):
 
 
 
-    ###########################
-    # Create Season params    #
-    ###########################
-
-    ##inputs
-    date_initiate_z = pinp.f_seasonal_inp(pinp.general['i_date_initiate_z'], numpy=True, axis=0).astype('datetime64')
-    date_initiate_zidaebxyg = fun.f_expand(date_initiate_z, z_pos)
-    index_zidaebxyg = fun.f_expand(index_z, z_pos)
-    start_of_season_zidaebxyg = date_node_zidaebxygm[...,0]
-    end_of_season_zidaebxyg = start_of_season_zidaebxyg + np.timedelta64(364,'D') #use 364 because end date is the day before brk.
-
-    ##parent z
-    date_prev_node_zidaebxygm = np.roll(date_node_zidaebxygm, axis=-1, shift=1)
-    existing_season_prev_zidaebxygm = np.maximum.accumulate((date_prev_node_zidaebxygm==date_initiate_zidaebxyg[...,na]) * index_zidaebxyg[...,na], axis=0)
-    existing_season_prev_zidaebxygm[:,...,0] = np.maximum.accumulate((date_node_zidaebxygm==date_initiate_zidaebxyg[...,na]) * index_zidaebxyg[...,na], axis=0)[:,...,0] #the perent at the first node are the seasons identified by break
-    parent_zidaebxyg = np.max(np.maximum.accumulate(existing_season_prev_zidaebxygm * (date_node_zidaebxygm==date_initiate_zidaebxyg[...,na]), axis=0),axis=-1)
-    parent_idaebxygz9 = np.moveaxis(parent_zidaebxyg, source=0, destination=-1)
-
-    ##number req mask. Each z8 always requires from the same z9 season eg z8[1] requires from z9[1]
-    identity_z8ida0e0b0xygz9 = fun.f_expand(np.identity(len_z),z_pos-1, right_pos=-1)
-    mask_param_reqz8z9_z8z9 = identity_z8ida0e0b0xygz9
-
-    ##dams child parent transfer
-    ###adjust dvp start dates to the base yr (dates must be between break of current season and break of next season)
-    add_yrs = np.ceil(np.maximum(0,(start_of_season_zidaebxyg - dvp_start_va1e1b1nwzida0e0b0xyg1).astype('timedelta64[D]').astype(int) / 365))
-    sub_yrs = np.ceil(np.maximum(0,(dvp_start_va1e1b1nwzida0e0b0xyg1 - end_of_season_zidaebxyg).astype('timedelta64[D]').astype(int) / 365))
-    date_nodes_va1e1b1nwzida0e0b0xyg1 = dvp_start_va1e1b1nwzida0e0b0xyg1 + add_yrs * np.timedelta64(365, 'D') - sub_yrs * np.timedelta64(365, 'D')
-    ###mask when season is identified
-    #todo this mask should be applied to all params with an active v & z axis (same as mask_w8vars_va1e1b1nw8zida0e0b0xyg1)
-    mask_z8var_va1e1b1nwzida0e0b0xyg1 = date_initiate_zidaebxyg <= date_nodes_va1e1b1nwzida0e0b0xyg1
-    ###dams numbers prov mask. Parent seasons provide to child season until the child season is identified.
-    prov_self_va1e1b1nwzida0e0b0xyg1z9 = mask_z8var_va1e1b1nwzida0e0b0xyg1[...,na] * identity_z8ida0e0b0xyg1z9
-    prov_parent_va1e1b1nwzida0e0b0xyg1z9 = mask_z8var_va1e1b1nwzida0e0b0xyg1[...,na] * (index_zidaebxyg[...,na] == parent_idaebxygz9)
-    mask_z9var_va1e1b1nwzida0e0b0xyg1z = np.swapaxes(mask_z8var_va1e1b1nwzida0e0b0xyg1[...,na], z_pos-1, -1)
-    prov_parent_va1e1b1nwzida0e0b0xyg1z9 = prov_parent_va1e1b1nwzida0e0b0xyg1z9 * np.logical_not(mask_z9var_va1e1b1nwzida0e0b0xyg1z)
-    mask_param_provz8z9_va1e1b1nwzida0e0b0xyg1z9 = np.logical_or(prov_self_va1e1b1nwzida0e0b0xyg1z9, prov_parent_va1e1b1nwzida0e0b0xyg1z9)
-    ###cluster e and b (e axis is active from the dvp dates)
-    mask_param_provz8z9_k2tva1e1b1nwzida0e0b0xyg1z9 = np.sum(mask_param_provz8z9_va1e1b1nwzida0e0b0xyg1z9
-                                                          * (a_k2cluster_va1e1b1nwzida0e0b0xyg1==index_k2tva1e1b1nwzida0e0b0xyg1)[...,na],
-                                                          axis=(e1_pos-1,b1_pos-1), keepdims=True)
-
-
-    ##offs child parent transfer
-    ###adjust dvp start dates to the base yr (dates must be between break of current season and break of next season)
-    add_yrs = np.ceil(np.maximum(0,(start_of_season_zidaebxyg - dvp_start_va1e1b1nwzida0e0b0xyg3).astype('timedelta64[D]').astype(int) / 365))
-    sub_yrs = np.ceil(np.maximum(0,(dvp_start_va1e1b1nwzida0e0b0xyg3 - end_of_season_zidaebxyg).astype('timedelta64[D]').astype(int) / 365))
-    date_nodes_va1e1b1nwzida0e0b0xyg3 = dvp_start_va1e1b1nwzida0e0b0xyg3 + add_yrs * np.timedelta64(365, 'D') - sub_yrs * np.timedelta64(365, 'D')
-    ###mask when season is identified
-    #todo this mask should be applied to all params with an active v & z axis (same as mask_w8vars_va1e1b1nw8zida0e0b0xyg3)
-    mask_z8var_va1e1b1nwzida0e0b0xyg3 = date_initiate_zidaebxyg <= date_nodes_va1e1b1nwzida0e0b0xyg3
-    ###offs numbers prov mask. Parent seasons provide to child season until the child season is identified.
-    prov_self_va1e1b1nwzida0e0b0xyg3z9 = mask_z8var_va1e1b1nwzida0e0b0xyg3[...,na] * identity_z8ida0e0b0xyg3z9
-    prov_parent_va1e1b1nwzida0e0b0xyg3z9 = mask_z8var_va1e1b1nwzida0e0b0xyg3[...,na] * (index_zidaebxyg[...,na] == parent_idaebxygz9)
-    mask_z9var_va1e1b1nwzida0e0b0xyg3z = np.swapaxes(mask_z8var_va1e1b1nwzida0e0b0xyg3[...,na], z_pos-1, -1)
-    prov_parent_va1e1b1nwzida0e0b0xyg3z9 = prov_parent_va1e1b1nwzida0e0b0xyg3z9 * np.logical_not(mask_z9var_va1e1b1nwzida0e0b0xyg3z)
-    mask_param_provz8z9_va1e1b1nwzida0e0b0xyg3z9 = np.logical_or(prov_self_va1e1b1nwzida0e0b0xyg3z9, prov_parent_va1e1b1nwzida0e0b0xyg3z9)
-    ###cluster d (d axis is active from the dvp dates)
-    mask_param_provz8z9_k3k5tva1e1b1nwzida0e0b0xyg3z9 = np.sum(mask_param_provz8z9_va1e1b1nwzida0e0b0xyg3z9
-                                                          * (a_k3cluster_da0e0b0xyg3 == index_k3k5tva1e1b1nwzida0e0b0xyg3)[...,na],
-                                                          axis=d_pos-1, keepdims=True)
-
-
-    # mask_param_provz8z9_va1e1b1nwzida0e0b0xyg1z9 = np.logical_or(mask_z8var_va1e1b1nwzida0e0b0xyg1[...,na] * identity_z8ida0e0b0xyg1z9,
-    #                                                               mask_z8var_va1e1b1nwzida0e0b0xyg1[...,na] * (index_zidaebxyg[...,na] == parent_idaebxygz9))
-    # mask_param_reqz8z9_mz8z9 = mask_z8var_va1e1b1nwzida0e0b0xyg1[...,na] * identity_z8ida0e0b0xyg1z9
-
+    ####################################
+    #adjust params for season sequence #
+    ####################################
 
     ##dams numbers req and prov
-    currentdvp_is_seasonstart_va1e1b1nwzida0e0b0xyg1gw = dvp_type_va1e1b1nwzida0e0b0xyg1[...,na,na] == season_vtype1
-    nextdvp_is_seasonstart_va1e1b1nwzida0e0b0xyg1gw = dvp_type_next_va1e1b1nwzida0e0b0xyg1[...,na,na] == season_vtype1
+    ###cluster e&b (e axis is active from the dvp dates)
+    currentdvp_is_seasonstart_va1e1b1nwzida0e0b0xyg1 = np.sum(dvp_type_va1e1b1nwzida0e0b0xyg1 == season_vtype1
+                                                          * (a_k2cluster_va1e1b1nwzida0e0b0xyg1==index_k2tva1e1b1nwzida0e0b0xyg1),
+                                                          axis=(e1_pos,b1_pos), keepdims=True)
+    nextdvp_is_seasonstart_va1e1b1nwzida0e0b0xyg1 = np.sum(dvp_type_next_va1e1b1nwzida0e0b0xyg1 == season_vtype1
+                                                          * (a_k2cluster_va1e1b1nwzida0e0b0xyg1==index_k2tva1e1b1nwzida0e0b0xyg1),
+                                                          axis=(e1_pos,b1_pos), keepdims=True)
+
     ###within season transfer
-    numbers_reqwithin_dams_k28k29tva1e1b1nw8zida0e0b0xyg1g9w9 = numbers_req_dams_k28k29tva1e1b1nw8zida0e0b0xyg1g9w9 * np.logical_not(currentdvp_is_seasonstart_va1e1b1nwzida0e0b0xyg1gw)
-    numbers_provwithin_dams_k28k29tva1e1b1nw8zida0e0b0xyg1g9w9 = numbers_prov_dams_k28k29tva1e1b1nw8zida0e0b0xyg1g9w9 * np.logical_not(nextdvp_is_seasonstart_va1e1b1nwzida0e0b0xyg1gw)
-    numbers_provthiswithin_dams_k28k29tva1e1b1nw8zida0e0b0xyg1g9w9 = numbers_prov_dams_k28k29tva1e1b1nw8zida0e0b0xyg1g9w9 * np.logical_not(nextdvp_is_seasonstart_va1e1b1nwzida0e0b0xyg1gw)
+    numbers_reqwithin_dams_k28k29tva1e1b1nw8zida0e0b0xyg1g9w9 = numbers_req_dams_k28k29tva1e1b1nw8zida0e0b0xyg1g9w9 * np.logical_not(currentdvp_is_seasonstart_va1e1b1nwzida0e0b0xyg1[...,na,na])
+    numbers_provwithin_dams_k28k29tva1e1b1nw8zida0e0b0xyg1g9w9 = numbers_prov_dams_k28k29tva1e1b1nw8zida0e0b0xyg1g9w9 * np.logical_not(nextdvp_is_seasonstart_va1e1b1nwzida0e0b0xyg1[...,na,na])
+    numbers_provthiswithin_dams_k28k29tva1e1b1nw8zida0e0b0xyg1g9w9 = numbers_prov_dams_k28k29tva1e1b1nw8zida0e0b0xyg1g9w9 * np.logical_not(nextdvp_is_seasonstart_va1e1b1nwzida0e0b0xyg1[...,na,na])
     ###between season transfer
-    numbers_reqbetween_dams_k28k29tva1e1b1nw8zida0e0b0xyg1g9w9 = numbers_req_dams_k28k29tva1e1b1nw8zida0e0b0xyg1g9w9 * currentdvp_is_seasonstart_va1e1b1nwzida0e0b0xyg1gw
-    numbers_provbetween_dams_k28k29tva1e1b1nw8zida0e0b0xyg1g9w9 = numbers_prov_dams_k28k29tva1e1b1nw8zida0e0b0xyg1g9w9 * nextdvp_is_seasonstart_va1e1b1nwzida0e0b0xyg1gw
-    numbers_provthisbetween_dams_k28k29tva1e1b1nw8zida0e0b0xyg1g9w9 = numbers_prov_dams_k28k29tva1e1b1nw8zida0e0b0xyg1g9w9 * nextdvp_is_seasonstart_va1e1b1nwzida0e0b0xyg1gw
+    numbers_reqbetween_dams_k28k29tva1e1b1nw8zida0e0b0xyg1g9w9 = numbers_req_dams_k28k29tva1e1b1nw8zida0e0b0xyg1g9w9 * currentdvp_is_seasonstart_va1e1b1nwzida0e0b0xyg1[...,na,na]
+    numbers_provbetween_dams_k28k29tva1e1b1nw8zida0e0b0xyg1g9w9 = numbers_prov_dams_k28k29tva1e1b1nw8zida0e0b0xyg1g9w9 * nextdvp_is_seasonstart_va1e1b1nwzida0e0b0xyg1[...,na,na]
+    numbers_provthisbetween_dams_k28k29tva1e1b1nw8zida0e0b0xyg1g9w9 = numbers_prov_dams_k28k29tva1e1b1nw8zida0e0b0xyg1g9w9 * nextdvp_is_seasonstart_va1e1b1nwzida0e0b0xyg1[...,na,na]
 
     ##offs numbers req and prov
-    currentdvp_is_seasonstart_va1e1b1nwzida0e0b0xyg3w = dvp_type_va1e1b1nwzida0e0b0xyg3[...,na] == season_vtype3
-    nextdvp_is_seasonstart_va1e1b1nwzida0e0b0xyg3w = dvp_type_next_va1e1b1nwzida0e0b0xyg3[...,na] == season_vtype3
+    ###cluster d (d axis is active from the dvp dates)
+    currentdvp_is_seasonstart_k3k5tva1e1b1nwzida0e0b0xyg3 = np.sum(dvp_type_va1e1b1nwzida0e0b0xyg3 == season_vtype3
+                                                               * (a_k3cluster_da0e0b0xyg3 == index_k3k5tva1e1b1nwzida0e0b0xyg3),
+                                                               axis=d_pos,keepdims=True)
+    nextdvp_is_seasonstart_k3k5tva1e1b1nwzida0e0b0xyg3 = np.sum(dvp_type_next_va1e1b1nwzida0e0b0xyg3 == season_vtype3
+                                                               * (a_k3cluster_da0e0b0xyg3 == index_k3k5tva1e1b1nwzida0e0b0xyg3),
+                                                               axis=d_pos,keepdims=True)
+
     ###within season transfer
-    numbers_reqwithin_offs_k3k5tva1e1b1nw8zida0e0b0xygw9 = numbers_req_offs_k3k5tva1e1b1nw8zida0e0b0xygw9 * np.logical_not(currentdvp_is_seasonstart_va1e1b1nwzida0e0b0xyg3w)
-    numbers_provwithin_offs_k3k5tva1e1b1nw8zida0e0b0xygw9 = numbers_prov_offs_k3k5tva1e1b1nw8zida0e0b0xygw9 * np.logical_not(nextdvp_is_seasonstart_va1e1b1nwzida0e0b0xyg3w)
+    numbers_reqwithin_offs_k3k5tva1e1b1nw8zida0e0b0xygw9 = numbers_req_offs_k3k5tva1e1b1nw8zida0e0b0xygw9 * np.logical_not(currentdvp_is_seasonstart_k3k5tva1e1b1nwzida0e0b0xyg3[...,na])
+    numbers_provwithin_offs_k3k5tva1e1b1nw8zida0e0b0xygw9 = numbers_prov_offs_k3k5tva1e1b1nw8zida0e0b0xygw9 * np.logical_not(nextdvp_is_seasonstart_k3k5tva1e1b1nwzida0e0b0xyg3[...,na])
     ###between season transfer
-    numbers_reqbetween_offs_k3k5tva1e1b1nw8zida0e0b0xygw9 = numbers_req_offs_k3k5tva1e1b1nw8zida0e0b0xygw9 * currentdvp_is_seasonstart_va1e1b1nwzida0e0b0xyg3w
-    numbers_provbetween_offs_k3k5tva1e1b1nw8zida0e0b0xygw9 = numbers_prov_offs_k3k5tva1e1b1nw8zida0e0b0xygw9 * nextdvp_is_seasonstart_va1e1b1nwzida0e0b0xyg3w
+    numbers_reqbetween_offs_k3k5tva1e1b1nw8zida0e0b0xygw9 = numbers_req_offs_k3k5tva1e1b1nw8zida0e0b0xygw9 * currentdvp_is_seasonstart_k3k5tva1e1b1nwzida0e0b0xyg3[...,na]
+    numbers_provbetween_offs_k3k5tva1e1b1nw8zida0e0b0xygw9 = numbers_prov_offs_k3k5tva1e1b1nw8zida0e0b0xygw9 * nextdvp_is_seasonstart_k3k5tva1e1b1nwzida0e0b0xyg3[...,na]
+
+    #todo do i need to add prog2dams and prog2off here as well? (i think i do)
 
     ###########################
     #  report P2V             #
