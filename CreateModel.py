07a0175f0f8eab99424292dc3a05ebb1973a5c87
Michael <youngmr44@gmail.com>
2021-10-02 21:12:59 +0800

    simplify m set building

--- CreateModel.py
+++ CreateModel.py
@@ -103,12 +103,8 @@ def sets(model, nv):
     #rotation #
     ###########
 
-    ##nodes
-    if pinp.general['steady_state'] or np.count_nonzero(pinp.general['i_mask_z']) == 1:
-        len_m = 1 #if steady state then m axis is singleton.
-    else:
-        len_m = pinp.general['i_date_node_zm'].shape[-1] + 1 #+1 to account for dummy dry seeding period
-    model.s_phase_periods = Set(initialize=np.array(['m{0}'.format(i) for i in range(len_m)]),doc='season nodes')
+    ##phase periods
+    model.s_phase_periods = Set(initialize=per.f_phase_periods(keys=True),doc='season nodes')
 
     ##lmus
     lmu_mask = pinp.general['i_lmu_area'] > 0
