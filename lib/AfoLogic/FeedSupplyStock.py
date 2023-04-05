e6d41cfc84692603b2ed3b5a85ac981bbc7e575b
Nick Wylynko <nick.wylynko@innovation-i.com.au>
2023-04-05 06:17:23 +0000

    relative pkl file

--- lib/AfoLogic/FeedSupplyStock.py
+++ lib/AfoLogic/FeedSupplyStock.py
@@ -691,5 +691,6 @@ def f1_pkl_feedsupply(lp_vars,r_vals,pkl_fs_info):
         fs_create_number = sinp.structuralsa['i_fs_create_number']
         # directory_path = os.path.dirname(os.path.abspath(__file__))  # path of directory - required when exp is run from a different location (e.g. in the web app)
         # with open(os.path.join(directory_path, 'pkl/pkl_fs{0}.pkl'.format(fs_create_number)),"wb") as f:
-        with open('pkl/pkl_fs{0}.pkl'.format(fs_create_number),"wb") as f:
+        pkl_fs = relativeFile.find(__file__, "../../pkl", 'pkl_fs{0}.pkl'.format(fs_create_number))
+        with open(pkl_fs, "wb") as f:
             pkl.dump(pkl_fs, f)
