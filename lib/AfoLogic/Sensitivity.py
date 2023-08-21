b4d7fbde2fedc9d67fe248f30b970a56cfe26420
Michael <youngmr44@gmail.com>
2023-08-21 20:56:50 +0800

    add a bound to constrain total dam numbers at prejoining

--- lib/AfoLogic/Sensitivity.py
+++ lib/AfoLogic/Sensitivity.py
@@ -456,6 +456,7 @@ def create_sa():
     sav['est_drys_retained_birth_o'] = np.full(pinp.sheep['i_drys_retained_birth_est_o'].shape, '-', dtype=object)   #Estimate of the propn of drys sold at birth
     sav['bnd_sale_twice_dry_inc'] = '-'   #SA to include the bound which forces twice dry dams to be sold
     sav['bnd_twice_dry_propn'] = '-'   #SA to change twice dry dam proportion
+    sav['bnd_total_dams'] = '-'   #control the total number of dams at prejoining
     sav['bnd_lo_dam_inc'] = '-'   #control if dam lower bound is on.
     sav['bnd_lo_dams_tog1'] = np.full((len_t1,) + (len_d,) + (len_g1,), '-', dtype=object)   #min number of dams
     sav['bnd_lo_dams_tVg1'] = np.full((len_t1,) + (len_V,) + (len_g1,), '-', dtype=object)   #min number of dams
