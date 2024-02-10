43ed936a4dd281452bf05e4e4c6d09a808cc097c
Michael <youngmr44@gmail.com>
2024-02-10 12:35:41 +0800

    Change reporting defaults for feedbudget total report. So it works for web app.

--- lib/AfoLogic/ReportControl.py
+++ lib/AfoLogic/ReportControl.py
@@ -116,14 +116,14 @@ def f_run_report(lp_vars, r_vals, report_run, trial_name, infeasible = None, use
     if report_run.loc['run_feedbudget', 'Run']:
         option = f_update_default_controls(user_controls, 'feed', 'option', 0)  #0 mei/hd/day & propn from each source, 1 total mei
         nv_option = f_update_default_controls(user_controls, 'feed', 'nv_option', 0)   #0 Separate NV pool, NV pool summed.
-        residue_cols = f_update_default_controls(user_controls, 'feed', 'residue_cols', [7]) #biomass use
+        residue_cols = f_update_default_controls(user_controls, 'feed', 'residue_cols', [])
         dams_cols = f_update_default_controls(user_controls, 'feed', 'dams_cols', [6]) #birth opp
         offs_cols = f_update_default_controls(user_controls, 'feed', 'offs_cols', [7]) #shear opp
         reports["feed"] = rfun.f_feed_budget(lp_vars, r_vals, option=option, nv_option=nv_option, dams_cols=dams_cols, offs_cols=offs_cols, residue_cols=residue_cols)
     if report_run.loc['run_feedbudget', 'Run']:
         option = f_update_default_controls(user_controls, 'feed_total', 'option', 1)  #0 mei/hd/day & propn from each source, 1 total mei
         nv_option = f_update_default_controls(user_controls, 'feed_total', 'nv_option', 0)   #0 Separate NV pool, NV pool summed.
-        residue_cols = f_update_default_controls(user_controls, 'feed_total', 'residue_cols', [7]) #biomass use
+        residue_cols = f_update_default_controls(user_controls, 'feed_total', 'residue_cols', [])
         dams_cols = f_update_default_controls(user_controls, 'feed_total', 'dams_cols', [6]) #birth opp
         offs_cols = f_update_default_controls(user_controls, 'feed_total', 'offs_cols', [7]) #shear opp
         reports["feed_total"] = rfun.f_feed_budget(lp_vars, r_vals, option=option, nv_option=nv_option, dams_cols=dams_cols, offs_cols=offs_cols, residue_cols=residue_cols)
