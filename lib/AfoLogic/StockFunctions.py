Take to Master: Fix errors in the f_lwc_mu(), mostly associated with the back calculation of mei from change in tissue mass. With related changes in f_lwc_nfs()

lib/AfoLogic/StockFunctions.py
@@ -1628,22 +1628,22 @@ def f_lwc_cs(cg, rc_start, mei, mem, mew, zf1, zf2, kg, rev_trait_value, mec = 0
     ebg = f1_rev_update('lwc', ebg, rev_trait_value)
     ##scale fat, muscle & viscera weight gain to match ebg (required because energy might not tally & because of the REV adjustment)
     scalar = fun.f_divide(ebg, d_fat + d_muscle + d_viscera)
-    d_fat *= scalar
-    d_muscle *= scalar
-    d_viscera *= scalar
+    d_fat = d_fat * scalar
+    d_muscle = d_muscle * scalar
+    d_viscera = d_viscera * scalar
     ##Note: in the CSIRO equation system fat, muscle and viscera are just for reporting purposes
     return ebg, evg, d_fat, d_muscle, d_viscera, surplus_energy
 
 
-def f_lwc_mu(cg, rc_start, mei, meme, mew, new, zf1, zf2, kge, kf, kp, heat_loss_m0p1, rev_trait_value
+def f_lwc_mu(cg, rc_start, mei_initial, meme, mew, new, zf1, zf2, kge, kf, kp, heat_loss_m0p1, rev_trait_value
              , mec = 0, nec = 0, mel = 0, nel = 0, gest_propn = 0, lact_propn = 0):
     #uses energy & efficiency approach like CSIRO but separates kf & kp and calculates proportion of fat & protein from mass and energy balance
     ## ME requirement to maintain maternal body energy (maintenance). Surplus is available for maternal body gain
     maintenance = meme + mec * gest_propn + mel * lact_propn + mew
     ##Level of feeding (maint = 0) Note: level is calculated elsewhere (differently) for use in Blaxter & Clapperton equations
-    level = (mei / maintenance) - 1
+    level = (mei_initial / maintenance) - 1
     ##Energy intake that is surplus to maintenance
-    surplus_energy = mei - maintenance
+    surplus_energy = mei_initial - maintenance
     below_maintenance = surplus_energy < 0
     ##Energy Value of gain as calculated.
     evg = cg[8, ...] - zf1 * (cg[9, ...] - cg[10, ...] * (level - 1)) + zf2 * cg[11, ...] * (rc_start - 1)
@@ -1664,10 +1664,11 @@ def f_lwc_mu(cg, rc_start, mei, meme, mew, new, zf1, zf2, kge, kf, kp, heat_loss
     neg = kg * surplus_energy
     ##Calculate the chill increment if heat_loss is greater than heat production
     retained_energy = neg + new + gest_propn * nec + lact_propn * nel
-    hp_total = mei - retained_energy
+    hp_total = mei_initial - retained_energy
     chill_increment = np.average(np.maximum(0, heat_loss_m0p1 - hp_total[..., na, na]), axis = (-1,-2))
+    #hp_total = hp_total + chill_increment
     ## increase mem to include the chill increment and reduce neg
-#    mem = meme - chill_increment
+    mem = meme + chill_increment
     neg = neg - chill_increment   #the chill increment is net energy so scaling by efficiency is not required
     ##Recalculate kg in case the chill increment is leading to weight loss
     kg = np.where(neg < 0, kge, 1 / (fat_propn / kf + prot_propn / kp))
@@ -1700,9 +1701,9 @@ def f_lwc_mu(cg, rc_start, mei, meme, mew, new, zf1, zf2, kge, kf, kp, heat_loss
         d_muscle = f1_weight_energy_conversion(cg, 1, energy=nemuscle)
         d_viscera = f1_weight_energy_conversion(cg, 2, energy=neviscera)
         scalar = fun.f_divide(ebg, d_fat + d_muscle + d_viscera)
-        nefat *= scalar
-        nemuscle *= scalar
-        neviscera *= scalar
+        nefat = nefat * scalar
+        nemuscle = nemuscle * scalar
+        neviscera = neviscera * scalar
         mefat = nefat / kf
         memuscle = nemuscle / kp
         meviscera = neviscera / kp
@@ -1714,7 +1715,7 @@ def f_lwc_mu(cg, rc_start, mei, meme, mew, new, zf1, zf2, kge, kf, kp, heat_loss
     ##Step 11: Back calculate MEI from the components as updated by the REVs
     ### calculated with a minimum heat production (heat_loss_m0p1) to cover situations when the REVs reduce hp.
     retained_energy = nefat + nemuscle + neviscera + new + gest_propn * nec + lact_propn * nel
-    metabolisable_energy = mefat + memuscle + meviscera + mew + gest_propn * mec + lact_propn * mel
+    metabolisable_energy = mem + mefat + memuscle + meviscera + mew + gest_propn * mec + lact_propn * mel
     mei = np.average(np.maximum(metabolisable_energy[...,na, na]
                                 , retained_energy[..., na, na] + heat_loss_m0p1), axis = (-1,-2))
 
@@ -1724,11 +1725,13 @@ def f_lwc_mu(cg, rc_start, mei, meme, mew, new, zf1, zf2, kge, kf, kp, heat_loss
     ###mem: maintenance energy requirement including the increment for heat loss (excluding conceptus growth & lactation)
     ### (MEI minus energy stored plus the associated HP for product formation and HAF)
     mem = mei - metabolisable_energy
+    ### Calculate adjustment to mei to reflect the changes made by the REV or post calc SA
+    mei_adjustment = mei - mei_initial
 
-    return ebg, evg, d_fat, d_muscle, d_viscera, mei, hp_total, surplus_energy, kg, mem
+    return ebg, evg, d_fat, d_muscle, d_viscera, mei_adjustment, hp_total, surplus_energy, kg, mem
 
 
-def f_lwc_nfs(cg, ck, muscle, viscera, muscle_target, mei, km, md, hp_maint, dw, hp_dw, heat_loss_m0p1, step
+def f_lwc_nfs(cg, ck, muscle, viscera, muscle_target, mei_initial, km, md, hp_maint, dw, hp_dw, heat_loss_m0p1, step
               , rev_trait_value, dc=0, hp_dc=0, dl=0, hp_dl=0, gest_propn = 0, lact_propn = 0):
     ##fat gain (MJ/d) is calculated using a formula derived from the Oddy etal 2023 paper (see Generator9:p16-17)
     ###The calculation is multistep because parameter values (bcm & bcf) depend on the sign of dm and df
@@ -1766,8 +1769,8 @@ def f_lwc_nfs(cg, ck, muscle, viscera, muscle_target, mei, km, md, hp_maint, dw,
 
     ##Step 1: Calculate viscera values
     ### Step 1a: calculate dv from alpha_v for day 0
-    alpha_v = np.maximum(0, cg[35, ...] * mei + cg[36, ...] * m**0.41 + cg[37, ...] * md)
-    # alpha_v = np.maximum(0, (0.5 - 0.02 * md + 0.014 * mei) * m ** 0.75)   #alternative equation proposed & rejected by Hutton 8May24 6:27am
+    alpha_v = np.maximum(0, cg[35, ...] * mei_initial + cg[36, ...] * m**0.41 + cg[37, ...] * md)
+    # alpha_v = np.maximum(0, (0.5 - 0.02 * md + 0.014 * mei_initial) * m ** 0.75)   #alternative equation proposed & rejected by Hutton 8May24 6:27am
     dv0 = pv * (alpha_v - v)
     ### Step 1b: estimate average dv across the duration of the step (because approaching an asymptote).
     ###Assumption is that alpha_v doesn't change during the step. Which is an imperfect assumption because
@@ -1780,11 +1783,11 @@ def f_lwc_nfs(cg, ck, muscle, viscera, muscle_target, mei, km, md, hp_maint, dw,
     ##Step 2: Calculate MEI when dm==0 (mei_dm0) using derived equation and set bpm to required value
     mei_dm0 = (hp_maint + hp_dv + hp_dw + gest_propn * hp_dc + lact_propn * hp_dl - e0 / pm
                - blf * (dv + dw + gest_propn * dc + lact_propn * dl + e0 / pm))
-    bcm = np.where(mei > mei_dm0, ck[21, ...], ck[27, ...])
+    bcm = np.where(mei_initial > mei_dm0, ck[21, ...], ck[27, ...])
 
     ##Step 3: Calculate fat
     ###Step 3a: Calculate the numerator of df and set bf to required value
-    df_numerator = ((1 - pm * M) * (mei - (hp_maint + hp_dv + hp_dw + gest_propn * hp_dc + lact_propn * hp_dl + bcm * e0 * M))
+    df_numerator = ((1 - pm * M) * (mei_initial - (hp_maint + hp_dv + hp_dw + gest_propn * hp_dc + lact_propn * hp_dl + bcm * e0 * M))
                     - (1 + bcm * pm * M) * (dv + dw + gest_propn * dc + lact_propn * dl + e0 * M))
     bcf = np.where(df_numerator > 0, ck[20, ...], ck[26, ...])
     ###Step 3b: Calculate the denominator of df
@@ -1795,14 +1798,14 @@ def f_lwc_nfs(cg, ck, muscle, viscera, muscle_target, mei, km, md, hp_maint, dw,
 
     ##Step 4: Net energy gain (MJ/d) (without heat loss included)
     neg_wo = (dfwo + dv + dw + gest_propn * dc + lact_propn * dl + e0 * M) / (1 - pm * M)   #formula for NEG as the source of energy, excluding dm
-    neg_wo_check = ((mei - (hp_maint + hp_dv + hp_dw + gest_propn * hp_dc + lact_propn * hp_dl + hp_dfwo + bcm * e0 * M))
+    neg_wo_check = ((mei_initial - (hp_maint + hp_dv + hp_dw + gest_propn * hp_dc + lact_propn * hp_dl + hp_dfwo + bcm * e0 * M))
                     / (1 + bcm * pm * M)) #formula for NEG as the sink for energy, excluding dm
 
     ##Step 5: Total heat production (excluding the increase in HP to cover any extra required for chilling)
-    hp_total = mei - neg_wo
+    hp_total = mei_initial - neg_wo
 
     ##Step 6: Reduce neg if heat production is less than heat_loss (average of the m0p1 periods where heatloss is greater than heap production)
-    neg = np.average(np.minimum(neg_wo[..., na, na], mei[..., na, na] - heat_loss_m0p1), axis = (-1,-2))
+    neg = np.average(np.minimum(neg_wo[..., na, na], mei_initial[..., na, na] - heat_loss_m0p1), axis = (-1,-2))
 
     ##Step 7: Calculate muscle
     ##Step 7a: Change in muscle protein (MJ/d) for day 0
@@ -1846,9 +1849,9 @@ def f_lwc_nfs(cg, ck, muscle, viscera, muscle_target, mei, km, md, hp_maint, dw,
         d_muscle = f1_weight_energy_conversion(cg, 1, energy=dm)
         d_viscera = f1_weight_energy_conversion(cg, 2, energy=dv)
         scalar = fun.f_divide(ebg, d_fat + d_muscle + d_viscera)
-        df *= scalar
-        dm *= scalar
-        dv *= scalar
+        df = df * scalar
+        dm = dm * scalar
+        dv = dv * scalar
     ###Step 10c: Update weights after REV & scaling
     d_fat = f1_weight_energy_conversion(cg, 0, energy=df)
     d_muscle = f1_weight_energy_conversion(cg, 1, energy=dm)
@@ -1869,7 +1872,7 @@ def f_lwc_nfs(cg, ck, muscle, viscera, muscle_target, mei, km, md, hp_maint, dw,
     ### then adding it back in for the updated mei (using /(1-bmei))
     retained_energy = df + dm + dv + dw + gest_propn * dc + lact_propn * dl
     hp_re = hp_df + hp_dm + hp_dv + hp_dw + gest_propn * hp_dc + lact_propn * hp_dl
-    mei = np.average(np.maximum(((retained_energy + (hp_maint - mei * bmei) + hp_re)/(1 - bmei))[...,na, na]
+    mei = np.average(np.maximum(((retained_energy + (hp_maint - mei_initial * bmei) + hp_re)/(1 - bmei))[...,na, na]
                                 , retained_energy[..., na, na] + heat_loss_m0p1), axis = (-1,-2))
 
     ##Step 12: Calculate parameters for comparison with the CSIRO feeding standards
@@ -1882,8 +1885,10 @@ def f_lwc_nfs(cg, ck, muscle, viscera, muscle_target, mei, km, md, hp_maint, dw,
     ###mem: maintenance energy requirement including the increment for heat loss (excluding conceptus growth & lactation)
     ### (MEI minus energy stored plus the associated HP for product formation and HAF)
     mem = mei - (retained_energy + hp_re) / (1 - bmei)
+    ### Calculate adjustment to mei to reflect the changes made by the REV or post calc SA
+    mei_adjustment = mei - mei_initial
 
-    return ebg, evg, d_fat, d_muscle, d_viscera, mei, hp_total, surplus_energy, kg, mem
+    return ebg, evg, d_fat, d_muscle, d_viscera, mei_adjustment, hp_total, surplus_energy, kg, mem
 
 
 def f_wbe_mu(cg, fat, muscle, viscera=0):

lib/AfoLogic/StockGenerator.py
@@ -3798,9 +3798,9 @@ def generator(coefficients_c=[], params={}, r_vals={}, nv={}, pkl_fs_info={}, pk
                             new_sire = dw_sire
                             mew_sire = dw_sire + hp_dw_sire
                         if eqn_compare:
-                            r_compare17_q0q2tpsire[eqn_system, 0, :, p, ...] = d_cfw_sire    # temp0  using the fibre_cs values because fibre_nfs is not used
-                            r_compare17_q0q2tpsire[eqn_system, 1, :, p, ...] = d_fd_sire     # temp1  using the fibre_cs values because fibre_nfs is not used
-                            r_compare17_q0q2tpsire[eqn_system, 2, :, p, ...] = new_sire      # temp4  using the fibre_cs values because fibre_nfs is not used
+                            r_compare17_q0q2tpsire[eqn_system, 0, :, p, ...] = temp0    # temp0  using the fibre_cs values because fibre_nfs is not used
+                            r_compare17_q0q2tpsire[eqn_system, 1, :, p, ...] = temp1     # temp1  using the fibre_cs values because fibre_nfs is not used
+                            r_compare17_q0q2tpsire[eqn_system, 2, :, p, ...] = temp4      # temp4  using the fibre_cs values because fibre_nfs is not used
 
                     eqn_used = (eqn_used_g1_q1p[eqn_group, p] == eqn_system)
                     if (eqn_used or eqn_compare) and np.any(days_period_pa1e1b1nwzida0e0b0xyg1[p, ...] > 0):
@@ -3822,9 +3822,9 @@ def generator(coefficients_c=[], params={}, r_vals={}, nv={}, pkl_fs_info={}, pk
                             new_dams = dw_dams
                             mew_dams = dw_dams + hp_dw_dams
                         if eqn_compare:
-                            r_compare17_q0q2tpdams[eqn_system, 0, :, p, ...] = d_cfw_dams    # temp0  using the fibre_cs values because fibre_nfs is not used
-                            r_compare17_q0q2tpdams[eqn_system, 1, :, p, ...] = d_fd_dams     # temp1  using the fibre_cs values because fibre_nfs is not used
-                            r_compare17_q0q2tpdams[eqn_system, 2, :, p, ...] = new_dams      # temp4  using the fibre_cs values because fibre_nfs is not used
+                            r_compare17_q0q2tpdams[eqn_system, 0, :, p, ...] = temp0    # temp0  using the fibre_cs values because fibre_nfs is not used
+                            r_compare17_q0q2tpdams[eqn_system, 1, :, p, ...] = temp1     # temp1  using the fibre_cs values because fibre_nfs is not used
+                            r_compare17_q0q2tpdams[eqn_system, 2, :, p, ...] = temp4      # temp4  using the fibre_cs values because fibre_nfs is not used
 
                     eqn_used = (eqn_used_g3_q1p[eqn_group, p] == eqn_system)
                     if (eqn_used or eqn_compare) and np.any(days_period_pa1e1b1nwzida0e0b0xyg3[p, ...] > 0):
@@ -3845,9 +3845,9 @@ def generator(coefficients_c=[], params={}, r_vals={}, nv={}, pkl_fs_info={}, pk
                             new_offs = dw_offs
                             mew_offs = dw_offs + hp_dw_offs
                         if eqn_compare:
-                            r_compare17_q0q2tpoffs[eqn_system, 0, :, p, ...] = d_cfw_offs    # temp0  using the fibre_cs values because fibre_nfs is not used
-                            r_compare17_q0q2tpoffs[eqn_system, 1, :, p, ...] = d_fd_offs     # temp1  using the fibre_cs values because fibre_nfs is not used
-                            r_compare17_q0q2tpoffs[eqn_system, 2, :, p, ...] = new_offs      # temp4  using the fibre_cs values because fibre_nfs is not used
+                            r_compare17_q0q2tpoffs[eqn_system, 0, :, p, ...] = temp0    # temp0  using the fibre_cs values because fibre_nfs is not used
+                            r_compare17_q0q2tpoffs[eqn_system, 1, :, p, ...] = temp1     # temp1  using the fibre_cs values because fibre_nfs is not used
+                            r_compare17_q0q2tpoffs[eqn_system, 2, :, p, ...] = temp4      # temp4  using the fibre_cs values because fibre_nfs is not used
 
 
                 ##total heat production (excluding chill) & energy to offset chilling
@@ -4100,8 +4100,9 @@ def generator(coefficients_c=[], params={}, r_vals={}, nv={}, pkl_fs_info={}, pk
                             d_fat_sire = temp2
                             d_muscle_sire = temp3
                             d_viscera_sire = temp4
-                            pi_sire = pi_sire * fun.f_divide(temp5, mei_sire)  #scale pi by the variation in mei that results from REV changes.
-                            mei_sire = temp5
+                            pi_sire = pi_sire * (1 + fun.f_divide(temp5, mei_sire))  #scale pi by the variation in mei that results from REV changes.
+                            mei_sire = mei_sire + temp5
+                            mei_solid_sire = mei_solid_sire + temp5
                             surplus_energy_sire = temp7
                             mem_sire = temp9  #will overwrite the CSIRO version if NFS system is being used.
                         if eqn_compare:
@@ -4112,6 +4113,16 @@ def generator(coefficients_c=[], params={}, r_vals={}, nv={}, pkl_fs_info={}, pk
                             r_compare7_q0q2tpsire[eqn_system, 4, :, p, ...] = temp3
                             r_compare7_q0q2tpsire[eqn_system, 5, :, p, ...] = temp4
                             r_compare7_q0q2tpsire[eqn_system, 6, :, p, ...] = temp0
+                        temp0 = sfun.f1_level_nfs(mei_sire, hp_maint_sire)   #todo hp_maint is not the same as level of intake for RE == 0 because it include hp associated with the current level of intake (hp_mei)
+                        if eqn_used:
+                            level_sire = temp0
+                        temp0, temp1 = sfun.f_templc(cc_sire, ffcfw_start_sire, rc_start_sire, sl_start_sire, hp_total_sire
+                                                   , temp_ave_pa1e1b1nwzida0e0b0xyg[p], temp_max_pa1e1b1nwzida0e0b0xyg[p]
+                                                   , temp_min_pa1e1b1nwzida0e0b0xyg[p], ws_pa1e1b1nwzida0e0b0xyg[p]
+                                                   , rain_pa1e1b1nwzida0e0b0xygp0[p], index_m0)
+                        if eqn_used:
+                            temp_lc_sire = temp0  #temp1 not required here
+
                     ###dams
                     eqn_used = (eqn_used_g1_q1p[eqn_group, p] == eqn_system)
                     if (eqn_used or eqn_compare) and np.any(days_period_pa1e1b1nwzida0e0b0xyg1[p,...] >0):
@@ -4135,10 +4146,21 @@ def generator(coefficients_c=[], params={}, r_vals={}, nv={}, pkl_fs_info={}, pk
                             d_fat_dams = temp2
                             d_muscle_dams = temp3
                             d_viscera_dams = temp4
-                            pi_dams = pi_dams * fun.f_divide(temp5, mei_dams)  #scale pi by the variation in mei that results from REV changes.
-                            mei_dams = temp5
+                            pi_dams = pi_dams * (1 + fun.f_divide(temp5, mei_dams))  #scale pi by the variation in mei that results from REV changes.
+                            mei_dams = mei_dams + temp5
+                            mei_solid_dams = mei_solid_dams + temp5
                             surplus_energy_dams = temp7
                             mem_dams = temp9  #will overwrite the CSIRO version if NFS system is being used. Only used for post-loop SA.
+                        temp0 = sfun.f1_level_nfs(mei_dams, hp_maint_dams)
+                        if eqn_used:
+                            level_dams = temp0
+                        temp0, temp1 = sfun.f_templc(cc_dams, ffcfw_start_dams, rc_start_dams, sl_start_dams, hp_total_dams
+                                                     , temp_ave_pa1e1b1nwzida0e0b0xyg[p], temp_max_pa1e1b1nwzida0e0b0xyg[p]
+                                                     , temp_min_pa1e1b1nwzida0e0b0xyg[p], ws_pa1e1b1nwzida0e0b0xyg[p]
+                                                     , rain_pa1e1b1nwzida0e0b0xygp0[p], index_m0)
+                        if eqn_used:
+                            temp_lc_dams = temp0  #temp1 not required here
+
                     ###offs
                     eqn_used = (eqn_used_g3_q1p[eqn_group, p] == eqn_system)
                     if (eqn_used or eqn_compare) and np.any(days_period_pa1e1b1nwzida0e0b0xyg3[p,...] >0):
@@ -4158,8 +4180,9 @@ def generator(coefficients_c=[], params={}, r_vals={}, nv={}, pkl_fs_info={}, pk
                             d_fat_offs = temp2
                             d_muscle_offs = temp3
                             d_viscera_offs = temp4
-                            pi_offs = pi_offs * fun.f_divide(temp5, mei_offs)  #scale pi by the variation in mei that results from REV changes.
-                            mei_offs = temp5
+                            pi_offs = pi_offs * (1 + fun.f_divide(temp5, mei_offs))  #scale pi by the variation in mei that results from REV changes.
+                            mei_offs = mei_offs + temp5
+                            mei_solid_offs = mei_solid_offs + temp5
                             surplus_energy_offs = temp7
                             mem_offs = temp9  #will overwrite the CSIRO version if NFS system is being used.
                         if eqn_compare:
@@ -4170,6 +4193,15 @@ def generator(coefficients_c=[], params={}, r_vals={}, nv={}, pkl_fs_info={}, pk
                             r_compare7_q0q2tpoffs[eqn_system, 4, :, p, ...] = temp3
                             r_compare7_q0q2tpoffs[eqn_system, 5, :, p, ...] = temp4
                             r_compare7_q0q2tpoffs[eqn_system, 6, :, p, ...] = temp0
+                        temp0 = sfun.f1_level_nfs(mei_offs, hp_maint_offs)
+                        if eqn_used:
+                            level_offs = temp0
+                        temp0, temp1 = sfun.f_templc(cc_offs, ffcfw_start_offs, rc_start_offs, sl_start_offs, hp_total_offs
+                                                     , temp_ave_pa1e1b1nwzida0e0b0xyg[p], temp_max_pa1e1b1nwzida0e0b0xyg[p]
+                                                     , temp_min_pa1e1b1nwzida0e0b0xyg[p], ws_pa1e1b1nwzida0e0b0xyg[p]
+                                                     , rain_pa1e1b1nwzida0e0b0xygp0[p], index_m0)
+                        if eqn_used:
+                            temp_lc_offs = temp0  #temp1 not required here
 
                 eqn_system = 2 # New Feeding Standards = 2
                 if uinp.sheep['i_eqn_exists_q0q1'][eqn_group, eqn_system]:  # proceed with call & assignment if this system exists for this group
@@ -4189,8 +4221,9 @@ def generator(coefficients_c=[], params={}, r_vals={}, nv={}, pkl_fs_info={}, pk
                             d_fat_sire = temp2
                             d_muscle_sire = temp3
                             d_viscera_sire = temp4
-                            pi_sire = pi_sire * fun.f_divide(temp5, mei_sire)  #scale pi by the variation in mei that results from REV changes.
-                            mei_sire = temp5
+                            pi_sire = pi_sire * (1 + fun.f_divide(temp5, mei_sire))  #scale pi by the variation in mei that results from REV changes.
+                            mei_sire = mei_sire + temp5
+                            mei_solid_sire = mei_solid_sire + temp5
                             surplus_energy_sire = temp7
                             mem_sire = temp9 #will overwrite the CSIRO version if NFS system is being used.
                         if eqn_compare:
@@ -4234,8 +4267,9 @@ def generator(coefficients_c=[], params={}, r_vals={}, nv={}, pkl_fs_info={}, pk
                             d_fat_dams = temp2
                             d_muscle_dams = temp3
                             d_viscera_dams = temp4
-                            pi_dams = pi_dams * fun.f_divide(temp5, mei_dams)  #scale pi by the variation in mei that results from REV changes.
-                            mei_dams = temp5
+                            pi_dams = pi_dams * (1 + fun.f_divide(temp5, mei_dams))  #scale pi by the variation in mei that results from REV changes.
+                            mei_dams = mei_dams + temp5
+                            mei_solid_dams = mei_solid_dams + temp5
                             surplus_energy_dams = temp7
                             mem_dams = temp9 #will overwrite the CSIRO version if NFS system is being used. Only used for post-loop SA.
                         if eqn_compare:
@@ -4277,8 +4311,9 @@ def generator(coefficients_c=[], params={}, r_vals={}, nv={}, pkl_fs_info={}, pk
                             d_fat_offs = temp2
                             d_muscle_offs = temp3
                             d_viscera_offs = temp4
-                            pi_offs = pi_offs * fun.f_divide(temp5, mei_offs)  #scale pi by the variation in mei that results from REV changes.
-                            mei_offs = temp5
+                            pi_offs = pi_offs * (1 + fun.f_divide(temp5, mei_offs))  #scale pi by the variation in mei that results from REV changes.
+                            mei_offs = mei_offs + temp5
+                            mei_solid_offs = mei_solid_offs + temp5
                             surplus_energy_offs = temp7
                             mem_offs = temp9 #will overwrite the CSIRO version if NFS system is being used.
                         if eqn_compare:
@@ -4841,9 +4876,9 @@ def generator(coefficients_c=[], params={}, r_vals={}, nv={}, pkl_fs_info={}, pk
                                        , kg_fodd_yatf, mei_propn_herb_yatf)
                     if eqn_used:
                         kg_yatf = temp0  #efficiency from the CSIRO equations (for post calc SA)
-                    temp0, temp1, temp2, temp3, temp4, temp5, temp6, temp7, temp8, temp9 = sfun.f_lwc_mu(cg_yatf, rc_start_yatf, mei_yatf
-                                        , meme_yatf, mew_yatf, new_yatf, zf1_yatf, zf2_yatf, kg_yatf, kf_yatf, kp_yatf
-                                        , heat_loss_yatfm0p1, rev_trait_values['yatf'][p])
+                    temp0, temp1, temp2, temp3, temp4, temp5, temp6, temp7, temp8, temp9 = sfun.f_lwc_mu(cg_yatf
+                                        , rc_start_yatf, mei_yatf, meme_yatf, mew_yatf, new_yatf, zf1_yatf, zf2_yatf
+                                        , kg_yatf, kf_yatf, kp_yatf, heat_loss_yatfm0p1, rev_trait_values['yatf'][p])
                     hp_total_yatf = temp6
                     kg_yatf = temp8  #efficiency resulting from the NFS equations (for r_compare)
                     if eqn_used:
@@ -4852,8 +4887,9 @@ def generator(coefficients_c=[], params={}, r_vals={}, nv={}, pkl_fs_info={}, pk
                         d_fat_yatf = temp2
                         d_muscle_yatf = temp3
                         d_viscera_yatf = temp4
-                        pi_yatf = pi_yatf * fun.f_divide(temp5, mei_yatf)  #scale pi by the variation in mei that results from REV changes.
-                        mei_yatf = temp5
+                        pi_yatf = pi_yatf * (1 + fun.f_divide(temp5, mei_yatf))  #scale pi by the variation in mei that results from REV changes.
+                        mei_yatf = mei_yatf + temp5
+                        mei_solid_yatf = mei_solid_yatf + temp5
                         surplus_energy_yatf = temp7
                         mem_yatf = temp9  #will overwrite the CSIRO version if NFS system is being used.
                     if eqn_compare:
@@ -4864,6 +4900,15 @@ def generator(coefficients_c=[], params={}, r_vals={}, nv={}, pkl_fs_info={}, pk
                         r_compare7_q0q2tpyatf[eqn_system, 4, :, p, ...] = temp3
                         r_compare7_q0q2tpyatf[eqn_system, 5, :, p, ...] = temp4
                         r_compare7_q0q2tpyatf[eqn_system, 6, :, p, ...] = temp0
+                    temp0 = sfun.f1_level_nfs(mei_yatf, hp_maint_yatf)
+                    if eqn_used:
+                        level_yatf = temp0
+                    temp0, temp1 = sfun.f_templc(cc_yatf, ffcfw_start_yatf, rc_start_yatf, sl_start_yatf, hp_total_yatf
+                                                 , temp_ave_pa1e1b1nwzida0e0b0xyg[p], temp_max_pa1e1b1nwzida0e0b0xyg[p]
+                                                 , temp_min_pa1e1b1nwzida0e0b0xyg[p], ws_pa1e1b1nwzida0e0b0xyg[p]
+                                                 , rain_pa1e1b1nwzida0e0b0xygp0[p], index_m0)
+                    if eqn_used:
+                        temp_lc_yatf = temp0  #temp1 not required here
 
             eqn_system = 2 # New Feeding Standards = 2
             if uinp.sheep['i_eqn_exists_q0q1'][eqn_group, eqn_system]:  # proceed with call & assignment if this system exists for this group
@@ -4882,8 +4927,9 @@ def generator(coefficients_c=[], params={}, r_vals={}, nv={}, pkl_fs_info={}, pk
                         d_fat_yatf = temp2
                         d_muscle_yatf = temp3
                         d_viscera_yatf = temp4
-                        pi_yatf = pi_yatf * fun.f_divide(temp5, mei_yatf)  #scale pi by the variation in mei that results from REV changes.
-                        mei_yatf = temp5
+                        pi_yatf = pi_yatf * (1 + fun.f_divide(temp5, mei_yatf))  #scale pi by the variation in mei that results from REV changes.
+                        mei_yatf = mei_yatf + temp5
+                        mei_solid_yatf = mei_solid_yatf + temp5
                         surplus_energy_yatf = temp7
                         mem_yatf = temp9 #will overwrite the CSIRO version if NFS system is being used.
                     if eqn_compare:
@@ -11077,7 +11123,7 @@ def generator(coefficients_c=[], params={}, r_vals={}, nv={}, pkl_fs_info={}, pk
         ##First define information about the array and the Excel file
         ###The array name and the slices to report are defined inside the try: except (in case they don't exist)
         ###One worksheet is created for each variable reported. To add more variables (or slices) include extra arrays, f_numpy2df() and .to_excel()
-        excel_filename = f'Wether_pm{1000*cg_offs[32,0,0]:.0f}_eo{1000*cg_offs[34,0,0]:.0f}.xlsx'
+        excel_filename = f'CompareMU.xlsx'    #f'Wether_pm{1000*cg_offs[32,0,0]:.0f}_eo{1000*cg_offs[34,0,0]:.0f}.xlsx'
         sheetname7_0 = 'mem'    #in NFS this includes HAF whereas HAF above the maintenance level of feeding is not included in CFS
         sheetname7_1 = 'hptotal'
         sheetname7_2 = 'kg'