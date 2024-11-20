Fix fodder trial in exp and fix bnd Hook up NFS in exp (formula wasn't coppied correctly).

lib/AfoLogic/BoundsPyomo.py
@@ -834,7 +834,7 @@ def f1_boundarypyomo_local(params, model):
         ###build bound if turned on
         if biomass_graze_bound_inc:
             ###setbound using ha of farm area
-            biomass_graze_bound_k1 = fun.f_sa(np.array([99999]), sen.sav['bnd_biomass_graze_k1'], 5)  # 99999 is arbitrary default value which mean skip constraint
+            biomass_graze_bound_k1 = fun.f_sa(np.array([99999]), sen.sav['bnd_biomass_graze_k1'][pinp.crop_landuse_mask_k1], 5)  # 99999 is arbitrary default value which mean skip constraint
             biomass_graze_bound_k1 = dict(zip(model.s_crops, biomass_graze_bound_k1))
 
             def k1_graze_bound(model, q, s, k1, z):