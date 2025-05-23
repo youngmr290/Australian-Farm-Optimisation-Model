6344103ec45a054486337bc84144831552938144
FSAS0 <john@farmingsystems.com.au>
2025-05-23 14:43:15 +0800

    Reduce popsize for the worker runs so that number of instances suits the google machine number of prcesses

--- lib/AfoLogic/SheepCalibration.py
+++ lib/AfoLogic/SheepCalibration.py
@@ -227,7 +227,7 @@ if __name__ == '__main__':
 
             ##Set some of the control variables (that might want to be tweaked later)
             maxiter = 400  #1000      The maximum number of iterations. # calls = (maxiter + 1) * selection population
-            popsize = 6  #15        The selection population is (popsize * n coefficients)
+            popsize = 5  #15        The selection population is (popsize * n coefficients)
             tol = 0.01  #0.01      The optimisation relative tolerance
             disp = True  #False     Display the result each iteration
             polish = False  #True      After the differential evolution carry out some further refining
