b99ac21b15faa6633235e35137afef38a1fb4b26
FSAS0 <john@farmingsystems.com.au>
2026-02-12 17:45:22 +0800

    Improve the FS combine module to handle slicing on 2 axes.

--- lib/AfoLogic/FS combine.py
+++ lib/AfoLogic/FS combine.py
@@ -1,3 +1,4 @@
+
 '''
 Combines two feed supplies to create a new fs. 
 Takes a slice/s from the source fs and overrides a slice/s in the destination fs. Then saves the resulting fs with a new fs_number.
@@ -37,17 +38,24 @@ generated the feed supply and the class of sheep that was copied.
             Twins 9
             Triplets A
 '''
+
+
 import pickle as pkl
 import sys
 import copy
 import ast
+import itertools
+import numpy as np
 
 import relativeFile
+
 
 ##setup
 source_axis_slice = {}
 dest_axis_slice = {}
 
+s_pos = -17
+t_pos = -16
 p_pos = -15
 a1_pos = -14
 e1_pos = -13
@@ -97,80 +105,81 @@ age_joined = [1, 2, 3, 4, 5, 6]
 # ###the new feed supply is if scanned for triplets but didn't change triplet nutrition.
 # ###source is twins b1[3] and destination is LSLN 33 b1[4], 32 b1[6], 31 b1[7] and 30 b1[10]
 # axis = b1_pos
-# dest_axis_slice[axis]   = [4, 5, 1], [6, 8, 1], [10, 11, 1]  # must be unique slices.
-# source_axis_slice[axis] = [3, 4, 1], [3, 4, 1], [3, 4, 1]  # same number of slices as destination (a slice can be repeated and copied to multiple destination slices).
+# dest_axis_slice[axis]   = [(4, 5, 1), (6, 8, 1), (10, 11, 1)]  # must be unique slices.
+# source_axis_slice[axis] = [(3, 4, 1), (3, 4, 1), (3, 4, 1)]  # same number of slices as destination (a slice can be repeated and copied to multiple destination slices).
 
-# the code below overwrites the not mated fs in the destination with NM from the source
-###source is NM b1[0] and destination is NM b1[0]
-copy_axis = b1_pos
-dest_axis_slice[copy_axis]   = [0, 1, 1], [0, 1, 1]  # can't be a single slide. If not uniques the later source slice will prevail.
-source_axis_slice[copy_axis] = [0, 1, 1], [0, 1, 1]  # same number of slices as destination (a slice can be repeated and copied to multiple destination slices).
+# the code below overwrites the mated fs of ewe lambs in the destination with mated fs from the source
+## the source is the fs for mated ewe lambs and the destination is the fs when ewe lambs are not mated (age_join==6)
+###source & destination is mated b1[1:] for the periods up to 2 tooth joining (0 to 100ish)
+copy_axis1 = b1_pos
+copy_axis2 = p_pos
+dest_axis_slice[copy_axis1]   = [(1, None, 1)]  # If not unique the later of the matching source slice will prevail. Can add more tuples to the list
+source_axis_slice[copy_axis1] = [(1, None, 1)]  # same number of slices as destination (a slice can be repeated and copied to multiple destination slices).
+
+# The date of 2 tooth joining and hence the slices for the p axis (copy_axis_2) vary with region and tol.
+p_slices = [[100,108,114],[102,107,112],[100,105,110]]
+
+count = 0
 
 #Loop through the 11 characteristics (defined above) to define the source, destination and new feedsupply
-count = 0
-for n_eqn in equations:
-    for n_os in off_sales:
-        for n_region in region:
-            for n_breed in breed:
-                for n_wbe in wbe:
-                    for n_tol in tol:
-                        for n_scan in scan:
-                            for n_mp in meat_price:
-                                for n_cg in crop_grazing:
-                                    for n_past in past_area:
-                                        for n_age in age_joined:
+param_lists = [equations, off_sales, region, breed, wbe, tol, scan, meat_price, crop_grazing, past_area, age_joined]
+for n_eqn, n_os, n_region, n_breed, n_wbe, n_tol, n_scan, n_mp, n_cg, n_past, n_age in itertools.product(*param_lists):
+    p_slice = p_slices[n_region-1][n_tol-1]
+    dest_axis_slice[copy_axis2] = [(0, p_slice, 1)]
+    source_axis_slice[copy_axis2] = [(0, p_slice, 1)]
 
-                                            fs_dest_number = (str(n_eqn) + str(n_os) + str(n_region) + str(n_breed)
-                                                              + f'{n_wbe:02d}' + str(n_tol) + str(n_scan) + str(n_mp)
-                                                              + str(n_cg) + str(n_past) + str(n_age))
-                                            ## 'Hard wire' the difference for the name of the source feedsupply in this code.
-                                            ## the code below uses age_joined 6 as the source
-                                            fs_source_number = (str(n_eqn) + str(n_os) + str(n_region) + str(n_breed)
-                                                                + f'{n_wbe:02d}' + str(n_tol) + str(n_scan) + str(n_mp)
-                                                                + str(n_cg) + str(n_past) + str(6))
-                                            ## 'Hard wire' the difference for the name of the new feedsupply in this code.
-                                            ## the code below appends '1' to the name
-                                            ## respective values from a scan 3 trial (7, 8 & 9). Triplets are based on the scan3 twin feed supply (9).
-                                            fs_new_number = (str(n_eqn) + str(n_os) + str(n_region) + str(n_breed)
-                                                             + f'{n_wbe:02d}' + str(n_tol) + str(n_scan) + str(n_mp)
-                                                             + str(n_cg) + str(n_past) + str(n_age)) + str(1)
+    ## 'Hard wire' the difference for the name of the destination feedsupply in this code.
+    ## the code below uses age_joined 6 as the destination
+    fs_dest_number = (str(n_eqn) + str(n_os) + str(n_region) + str(n_breed)
+                      + f'{n_wbe:02d}' + str(n_tol) + str(n_scan) + str(n_mp)
+                      + str(n_cg) + str(n_past) + str(6))
+    fs_source_number = (str(n_eqn) + str(n_os) + str(n_region) + str(n_breed)
+                        + f'{n_wbe:02d}' + str(n_tol) + str(n_scan) + str(n_mp)
+                        + str(n_cg) + str(n_past) + str(n_age))
+    ## The new name is similar to the source name with '2' appended
+    fs_new_number = (str(n_eqn) + str(n_os) + str(n_region) + str(n_breed)
+                     + f'{n_wbe:02d}' + str(n_tol) + str(n_scan) + str(n_mp)
+                     + str(n_cg) + str(n_past) + str(n_age)) + str(2)
 
-                                            #################
-                                            ## calcs        #
-                                            #################
+    #################
+    ## calcs        #
+    #################
 
-                                            ##read in both fs and retain the feeds supply for the target group
-                                            pkl_fs_path = relativeFile.find(__file__, "../../pkl", f"pkl_fs{fs_source_number}.pkl")
-                                            with open(pkl_fs_path, "rb") as f:
-                                                fs_source = pkl.load(f)['fs'][group]
-                                            pkl_fs_path = relativeFile.find(__file__, "../../pkl", f"pkl_fs{fs_dest_number}.pkl")
-                                            with open(pkl_fs_path, "rb") as f:
-                                                fs_dest = pkl.load(f)
-                                                # create a copy of the full dictionary prior to retaining only the target group
-                                                fs_new = copy.deepcopy(fs_dest)
-                                                fs_dest = fs_dest['fs'][group]
+    ##read in both fs and retain the feedsupply for the target group
+    pkl_fs_path = relativeFile.find(__file__, "../../pkl", f"pkl_fs{fs_source_number}.pkl")
+    with open(pkl_fs_path, "rb") as f:
+        fs_source = pkl.load(f)['fs'][group]
+    pkl_fs_path = relativeFile.find(__file__, "../../pkl", f"pkl_fs{fs_dest_number}.pkl")
+    with open(pkl_fs_path, "rb") as f:
+        full_dest = pkl.load(f)
+        fs_dest = full_dest['fs'][group]
+        # create a shallow copy of the full dictionary, then deep copy only the target array
+        fs_new = copy.copy(full_dest)
+        fs_new['fs'] = copy.copy(full_dest['fs'])
+        fs_dest = np.copy(fs_dest)  # work on a copy of the array to modify
 
-                                            ##manipulate fs to create new fs
-                                            for axis in source_axis_slice:
-                                                for slice_source, slice_dest in zip(source_axis_slice[axis], dest_axis_slice[axis]):
-                                                    sl_dest = [slice(None)] * fs_dest.ndim
-                                                    sl_source = [slice(None)] * fs_source.ndim
-                                                    start = slice_source[0]
-                                                    stop = slice_source[1]
-                                                    step = slice_source[2]
-                                                    sl_source[axis] = slice(start, stop, step)
-                                                    start = slice_dest[0]
-                                                    stop = slice_dest[1]
-                                                    step = slice_dest[2]
-                                                    sl_dest[axis] = slice(start, stop, step)
-                                                    ###update
-                                                    fs_dest[tuple(sl_dest)] = fs_source[tuple(sl_source)]
-                                            fs_new['fs'][group] = fs_dest
+    ##manipulate fs to create new fs
+    axes = list(source_axis_slice.keys())
+    if axes:
+        num_copies = len(source_axis_slice[axes[0]])
+        # All axes must have the same number of slice tuples otherwise this will throw and error
+        for i in range(num_copies):
+            sl_dest = [slice(None)] * fs_dest.ndim
+            sl_source = [slice(None)] * fs_source.ndim
+            for axis in axes:
+                start, stop, step = dest_axis_slice[axis][i]
+                sl_dest[axis] = slice(start, stop, step)
+                start, stop, step = source_axis_slice[axis][i]
+                sl_source[axis] = slice(start, stop, step)
+            ###update
+            fs_dest[tuple(sl_dest)] = fs_source[tuple(sl_source)]
+    fs_new['fs'][group] = fs_dest
 
-                                            ##save new fs
-                                            pkl_fs_path = relativeFile.find(__file__, "../../pkl", 'pkl_fs{0}.pkl'.format(fs_new_number))
-                                            with open(pkl_fs_path, "wb") as f:
-                                                pkl.dump(fs_new, f)
-                                            count = count + 1
+    ##save new fs
+    pkl_fs_path = relativeFile.find(__file__, "../../pkl", 'pkl_fs{0}.pkl'.format(fs_new_number))
+    with open(pkl_fs_path, "wb") as f:
+        pkl.dump(fs_new, f)
+    count = count + 1
 
 print(f'{count} new pkl_fs created.')
+
